c6a04548b4c34b7970be2f860488f58a72af8017
Areen <areen@astrotech.com>
2019-03-01 00:59:52 +0200

    Added method

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -11,5 +11,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private void pullDown(){}
+    private void pullDown(){ int i = 0;}
 }
